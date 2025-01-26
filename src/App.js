5788c39f86353f525f4ba35ca421ce7851095238
Sherin <68326500+Sherinsedalo@users.noreply.github.com>
2025-01-26 14:36:23 +0100

    correction: removed context wrap from App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import { ProductList } from "./components/Products/ProductList.js";
 import { ProductCard } from "./components/Products/ProductCard.js";
-
 import products from "./data";
 import "./App.css";
-import { CartProvider } from "./store-contexts/cart-context.js";
+
 import { Header } from "./components/Header/Header";
 
 function App() {
   return (
-    <CartProvider>
-      <div className="App">
-        <Header />
-        <ProductList>
-          {products.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-        </ProductList>
-      </div>
-    </CartProvider>
+    <div className="App">
+      <Header />
+      <ProductList>
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </ProductList>
+    </div>
   );
 }
 
